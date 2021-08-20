a2f8fbb59f9ad5978f4237f716ff42c8e27dd47b
Aliasgar <alisabun007@gmail.com>
2021-08-20 17:05:40 +0530

    Create modify_book.cpp

---
+++ modify_book.cpp
@@ -0,0 +1,60 @@
+void modify_book()
+{
+    char n[6];
+    int found=0;
+    clrscr();
+    cout<<"\n\n\tMODIFY BOOK REOCORD.... ";
+    cout<<"\n\n\tEnter The book no. of The book";
+    cin>>n;
+    fp.open("book.dat",ios::in|ios::out);
+    while(fp.read((char*)&bk,sizeof(book)) && found==0)
+    {
+        if(strcmpi(bk.retbno(),n)==0)
+        {
+            bk.show_book();
+            cout<<"\nEnter The New Details of book"<<endl;
+            bk.modify_book();
+            int pos=-1*sizeof(bk);
+                fp.seekp(pos,ios::cur);
+                fp.write((char*)&bk,sizeof(book));
+                cout<<"\n\n\t Record Updated";
+                found=1;
+        }
+    }
+
+        fp.close();
+        if(found==0)
+            cout<<"\n\n Record Not Found ";
+        getch();
+}
+
+
+void modify_student()
+{
+    char n[6];
+    int found=0;
+    clrscr();
+    cout<<"\n\n\tMODIFY STUDENT RECORD... ";
+    cout<<"\n\n\tEnter The admission no. of The student";
+    cin>>n;
+    fp.open("student.dat",ios::in|ios::out);
+    while(fp.read((char*)&st,sizeof(student)) && found==0)
+    {
+        if(strcmpi(st.retadmno(),n)==0)
+        {
+            st.show_student();
+            cout<<"\nEnter The New Details of student"<<endl;
+            st.modify_student();
+            int pos=-1*sizeof(st);
+            fp.seekp(pos,ios::cur);
+            fp.write((char*)&st,sizeof(student));
+            cout<<"\n\n\t Record Updated";
+            found=1;
+        }
+    }
+    
+    fp.close();
+    if(found==0)
+        cout<<"\n\n Record Not Found ";
+    getch();
+}
