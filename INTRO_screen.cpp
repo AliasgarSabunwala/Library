1e98aa2538b297fbb426892a0edf9c496debd36c
Aliasgar <alisabun007@gmail.com>
2021-08-20 16:37:09 +0530

    Update INTRO_screen.cpp

--- INTRO_screen.cpp
+++ INTRO_screen.cpp
@@ -7,7 +7,6 @@ void intro()
     cout<<"MANAGEMENT";
     gotoxy(35,17);
     cout<<"SYSTEM";
-    cout<<"\n\nMADE BY : YOUR NAME";
-    cout<<"\n\nSCHOOL : SCHOOL NAME";
+    cout<<"\n\nMADE BY : ALI ASGAR";
     getch();
 }
