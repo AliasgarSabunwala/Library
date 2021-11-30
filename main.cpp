89046f8226064bf6b113b03d9853b32bbd1290bb
Ali Asgar <ali.asgar2019@vitbhopal.ac.in>
2021-11-30 13:19:29 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// Calling Files
 header.h
 write_book.cpp
 modify_book.cpp
@@ -6,39 +7,49 @@ display_alls.cpp
 display_allb.cpp
 book_issue.cpp
 book_deposit.cpp
-intro.cpp
-admin_menu.cpp
+IntroSreen.cpp 
+Options.cpp
     
 // global declaration for stream object, object
 fstream fp,fp1;
 book bk;
 student st;
 
-void main()
+int main()
 {
     char ch;
+    
+    // Invoking IntroSreen.cpp 
     intro();
+    
     do
-    {
-        clrscr();
-        cout<<"\n\n\n\tMAIN MENU";
-        cout<<"\n\n\t01. BOOK ISSUE";
-        cout<<"\n\n\t02. BOOK DEPOSIT";
-          cout<<"\n\n\t03. ADMINISTRATOR MENU";
-          cout<<"\n\n\t04. EXIT";
-          cout<<"\n\n\tPlease Select Your Option (1-4) ";
-          ch=getche();
-          switch(ch)
-          {
-            case '1':clrscr();
-                 book_issue();
-                    break;
-              case '2':book_deposit();
-                     break;
-              case '3':admin_menu();
-                 break;
-              case '4':exit(0);
-              default :cout<<"\a";
-        }
-        }while(ch!='4');
+        {
+            clrscr();
+
+            // Options.cpp
+            cout<<"\n\n\n\t  Options";
+            cout<<"\n\n\t    01. BOOK ISSUE";
+            cout<<"\n\n\t    02. BOOK DEPOSIT";
+              cout<<"\n\n\t  03. ADMINISTRATOR MENU";
+              cout<<"\n\n\t  04. EXIT";
+              cout<<"\n\n\tPlease Select Your Option (1-4) ";
+
+              // getting Options
+              ch=getche();
+
+              // Using Switch Case
+              switch(ch)
+                  {
+                    case '1':clrscr();
+                         book_issue();
+                            break;
+                      case '2':book_deposit();
+                             break;
+                      case '3':admin_menu();
+                         break;
+                      case '4':exit(0);
+                      default :cout<<"Wrong Option";
+                  }
+            }
+        while(ch!='4');
 }
